cc7726c2343ea73b188cd7eec09cf5d6bf086ef0
Shashwat S <shashwat.singh11@gmail.com>
2022-01-04 17:00:26 +0530

    added kendo popup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+import { Popup } from '@progress/kendo-vue-popup';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    'k-popup': Popup,
+  },
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    onClick() {
+      this.show = !this.show;
+    }
+  }
+});
+
+</script>
+
+<template>
+  <div>
+    <button class="k-button" @click="onClick" ref="buttonWithPopup">Click Me</button>
+    <k-popup :anchor="'buttonWithPopup'" :show="show">Popup content.</k-popup>
+  </div>
+</template>
