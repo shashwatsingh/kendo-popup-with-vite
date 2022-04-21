d6c0c533c35a4794f1e677cfdb6a9fa8f16833d5
shashwatsingh <shashwat.singh11@gmail.com>
2022-04-21 20:06:01 +0530

    added dropdown

---
+++ dropdown/src/App.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import "@progress/kendo-theme-bootstrap/dist/all.css";
+
+import {
+  DropDownList,
+  DropDownListChangeEvent,
+} from "@progress/kendo-vue-dropdowns";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    "k-dropdown": DropDownList,
+  },
+  setup() {
+    const items = reactive([
+      {
+        ID: 100,
+        Name: 'Jill',
+      },
+      {
+        ID: 200,
+        Name: "Jack",
+      }
+    ]);
+    const changed = (e: DropDownListChangeEvent) => console.log('changed', e);
+
+    return {
+      items,
+      changed
+    }
+  }
+})
+</script>
+
+<template>
+  <main>
+    <h1>Test Kendo with Vite: DropDownList</h1>
+
+    <k-dropdown class="form-control" :data-items="items" :text-field="'Name'" :value-field="'ID'" @change="changed" />
+  </main>
+</template>
